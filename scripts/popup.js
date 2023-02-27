35109a7cfa3b3e62327a04a39688775d2ec06d9c
Igga96 <91895875+Igga96@users.noreply.github.com>
2023-02-28 00:57:49 +0300

    add new files and fix styles, links, and sections

--- scripts/popup.js
+++ scripts/popup.js
@@ -4,22 +4,23 @@ const overlay = document.querySelector('.overlay')
 const closeBtn = document.querySelector('.form__close')
 
 body = document.body
-openBtn.addEventListener('click', function(){
+openBtn.addEventListener('click', function () {
     openQuestionPopup()
+    menuList.classList.toggle('open')
 })
-function openQuestionPopup(){
-    body.classList.toggle ('noscroll')
+function openQuestionPopup() {
+    body.classList.toggle('noscroll')
     overlay.style.display = 'block'
     popupQuestion.style.display = 'block'
 }
-closeBtn.addEventListener('click', function(){
+closeBtn.addEventListener('click', function () {
     closePopup()
 })
-overlay.addEventListener('click', function(){
+overlay.addEventListener('click', function () {
     closePopup()
     closeMenuList()
 })
-function closePopup(){
+function closePopup() {
     body.classList.toggle('noscroll')
     overlay.style.display = 'none'
     popupQuestion.style.display = 'none'
